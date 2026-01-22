1d8e405654b17e6f57b9125b7f98d6b797707af1
sansar20174 <kumarsansar999@gmail.com>
2026-01-22 14:38:39 +0530

    first code

---
+++ script.js
@@ -0,0 +1,11 @@
+//const para=document.querySelectorAll("p")   
+//para[0].textContent="This is the updated text for the first paragraph."
+//para[1].textContent="This is the updated text for the second paragraph."
+//console.log(para)
+
+//const container = document.querySelector(".container");
+//container.innerHTML = "<h1> This is the heading </h1>";
+//console.log(container);8ydgfvyidygxfluyvrtu
+
+const btn = document.querySelector(".btn");
+btn.classList.add("btn");

891883c99c97eefa13c5542fe80acbe23d875199
sansar20174 <kumarsansar999@gmail.com>
2026-01-22 15:50:48 +0530

    `Added new code to script.js, including console logs, variable declarations, and function definitions`

--- script.js
+++ script.js
@@ -5,7 +5,44 @@
 
 //const container = document.querySelector(".container");
 //container.innerHTML = "<h1> This is the heading </h1>";
-//console.log(container);8ydgfvyidygxfluyvrtu
+//console.log(container);
 
-const btn = document.querySelector(".btn");
-btn.classList.add("btn");
+//const btn = document.querySelector(".btn");
+//btn.classList.add("btn");
+
+// debugger
+// console.log(a)
+// //console.log(b) 
+
+// var a=3453 // global scope for var
+// let b=4567 // script scope for let and const
+
+
+// console.log(a)
+// console.log(b)
+
+// function print(){
+//     let c = 7890
+//     console.log("Inside fn")
+// }
+
+// print();
+
+// function infinite(){
+//     infinite()
+// }
+// infinite()
+
+// console.log("a")
+// console.log("b") // TDZ (Temporal Dead Zone) for let and const
+
+// let a = 10
+// let b = 20
+
+let total = 345
+
+function calculate(){
+    console.log(total)
+    let total = 567
+}
+calculate()
